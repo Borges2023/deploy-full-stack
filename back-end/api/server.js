12c04963c35627e45a47ef3f3a52f25ebaf8ef4f
Borges2023 <tpowerinformatica.2016@gmail.com>
2025-03-06 19:12:01 -0300

    ajustes novos

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -3,21 +3,34 @@
 // CRUD DAS REQUISIÇÕES CREATE=POST, READ=GET, UPDATE=PUT, DELETE=DELETE
 // ENDPOINT = ROTA OU SEJA CAMINHO DENTRO DA API COMO /"CAMINHO"
 
-import exppress from "express";
+import exppress, { request, response } from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = exppress();
 const PORT = 3000;
 
-clearImmediateapp.use(cors());
+app.use(cors());
 
-app.get("/songs", async (request, response) => {
+app.get("/api/", (request, response) => {
+  response.send("endpoints `/artists´ e `/ongs´");
+});
+
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
+});
+
+app.use(exppress.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../front-end/dist/index.html"));
 });
 
 app.listen(PORT, () => {
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,9 @@
 // Fetch ou Axios = pega todas as requisições e manda para o artist
+import "dotenv/config";
 import axios from "axios";
 
-const URL = "http://localhost:3000";
+const { NODE_ENV } = process.env;
+const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
 // Abaixo o código requisita para pegar a informação no back-end
 const responseArtists = await axios.get(`${URL}/artists`);
