e5501a94c80f93c7626d22e0e9f5821a82df923a
Borges2023 <tpowerinformatica.2016@gmail.com>
2025-03-06 20:39:31 -0300

    ajuste de process atualização

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import logoSpotify from "../assets/logo/spotify-logo.png";
+import riffly from "../assets/logo/riffly.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
       <Link to="/">
-        <img src={logoSpotify} alt="Logo do Spotify" />
+        <img src={riffly} alt="Riffly" />
       </Link>
 
       <Link to="/" className="header__link">
-        <h1>Spotify</h1>
+        <h1>Riffly</h1>
       </Link>
     </div>
   );
