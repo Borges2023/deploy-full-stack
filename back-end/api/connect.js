979c5796ce00225720d2833850e19e904bb06989
Willian Borges <tpowerinformatica.2016@gmail.com>
2026-08-01 11:55:15 -0300

    Atualização de informações

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -3,8 +3,9 @@
 // Fullfilled
 import { MongoClient } from "mongodb";
 
-const URI =
-  "mongodb+srv://usuario_aqui:aqui@cluster0.4urkt5t.mongodb.net/?appName=Cluster0";
+//const URI =
+  //"mongodb+srv://usuario_aqui:aqui@cluster0.4urkt5t.mongodb.net/?appName=Cluster0";
+const URI = process.env.MONGODB_URI;
 
 const client = new MongoClient(URI);
 
